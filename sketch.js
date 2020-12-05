74868ec3805097bb8fba79479b6a0bb06753b3ec
Marvin Millar <dsmmarvini6@gmail.com>
2020-12-05 14:22:07 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    // put setup code here
+  }
+  
+  let lose;
+  let draw;
+  function preload(){
+      lose = loadSound('./Game Over (Sound Effect.mp3)')
+      endsound = loadSound('./whoosh sound effect.mp3')
+  }
+  
+  function draw() {
+    // put drawing code here
+  }
